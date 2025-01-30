51b81932876b1a07cf88c5cde85b6a7c8739e75c
Larskrs <lkssyv@gmail.com>
2025-01-30 09:50:14 +0100

    Results in a gay

--- src/components/common/LocalImage/index.jsx
+++ src/components/common/LocalImage/index.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 export default function LocalImage({ src, ...props }) {
-    if (process.env.NEXT_PUBLIC_URL?.startsWith("https://aktuelt.tv")) {
+    if (src?.startsWith(process.env.NEXT_PUBLIC_URL)) {
         src = src.replace("https://aktuelt.tv", "");
     }
 
