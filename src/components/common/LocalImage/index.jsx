a63dcf64cad81277e5cc11bd6c0d4fb4f7739f56
Larskrs <lkssyv@gmail.com>
2025-01-30 10:04:17 +0100

    udir

--- src/components/common/LocalImage/index.jsx
+++ src/components/common/LocalImage/index.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function LocalImage({ src, ...props }) {
     if (src?.startsWith("https://bamblingen.no")) {
-        src = src.replace("https://bamblingen.no", "");
+        src = src.replace("https://bamblingen.no", "http://localhost:3000");
     }
     if (src?.startsWith("https://aktuelt.tv")) {
         src = src.replace("https://aktuelt.tv", "");
