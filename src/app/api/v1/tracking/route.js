fe836bd940c3e29573341847e36eb0162a12a9c2
Larskrs <lkssyv@gmail.com>
2025-04-03 09:29:26 +0200

    Wish i could test this at home

--- src/app/api/v1/tracking/route.js
+++ src/app/api/v1/tracking/route.js
@@ -3,30 +3,31 @@ import logger from '../../../../../logger.mjs';
 import { db } from '@/lib/db';
 
 export async function POST(req) {
+    try {
 
-    // Hent X-Forwarded-For header (hvis satt)
-    const forwardedFor = req.headers.get('x-forwarded-for');
-    // Hvis headeren finnes, ta første IP (første element før evt. komma).
-    // Hvis den ikke finnes, fall tilbake til X-Real-IP eller '127.0.0.1'
-    var clientIp = forwardedFor 
-      ? forwardedFor.split(',')[0].trim()
-      : (req.headers.get('x-real-ip') ?? '127.0.0.1');
-
-    if (!clientIp || clientIp === "::1") {
-        clientIp = "8.8.8.8"
-    }
-
-    logger.info({message: "IP: " + clientIp})
-
-    const response = await fetch(`http://ip-api.com/json/${clientIp}`);
-    const data = await response.json();
-
-    logger.info({message: JSON.stringify(data)})
-
-    // TODO: Lagre data i en database
-    logger.info('Besøk fra:', `${data.countryName}, ${data.city}`);
-
-    const dbData = await db.visitor.create({
+        // Hent X-Forwarded-For header (hvis satt)
+        const forwardedFor = req.headers.get('x-forwarded-for');
+        // Hvis headeren finnes, ta første IP (første element før evt. komma).
+        // Hvis den ikke finnes, fall tilbake til X-Real-IP eller '127.0.0.1'
+        var clientIp = forwardedFor 
+        ? forwardedFor.split(',')[0].trim()
+        : (req.headers.get('x-real-ip') ?? '127.0.0.1');
+        
+        if (!clientIp || clientIp === "::1") {
+            clientIp = "8.8.8.8"
+        }
+        
+        logger.info({message: "IP: " + clientIp})
+        
+        const response = await fetch(`http://ip-api.com/json/${clientIp}`);
+        const data = await response.json();
+        
+        logger.info({message: JSON.stringify(data)})
+        
+        // TODO: Lagre data i en database
+        logger.info('Besøk fra:', `${data.countryName}, ${data.city}`);
+        
+        const dbData = await db.visitor.create({
         data: {
             countryName: data.countryName,
             countryCode: data.countryCode,
@@ -39,6 +40,9 @@ export async function POST(req) {
     })
     
     return NextResponse.json({ success: true, clientIp, dbData, location: `${data.country}, ${data.city}` })
+    } catch (err) {
+        return NextResponse.json({ success: false, error: err})
+    }
 
 }
 
